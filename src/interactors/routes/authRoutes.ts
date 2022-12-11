c0875b886e4a416a47a794947488ac39774e949a
Fabiojcp <fabio50019@gmail.com>
2022-12-11 18:26:53 -0300

    feat: auth body middleware

--- src/interactors/routes/authRoutes.ts
+++ src/interactors/routes/authRoutes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import authController from "../controller/authController";
+import authBodyMiddleware from "../middleware/body/authBodyMiddleware";
 
 const LoginRoutes = Router()
 
 // customer
-LoginRoutes.post("/", authController.customer)
+LoginRoutes.post("/", authBodyMiddleware.login, authController.customer)
 
 // Adm
-LoginRoutes.post("/management/", authController.manager)
+LoginRoutes.post("/management/", authBodyMiddleware.login, authController.manager)
 
 export default LoginRoutes
